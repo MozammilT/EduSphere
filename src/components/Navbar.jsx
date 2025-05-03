bd79318f73d9fd3f05c7d1a6aaafadfbd9bc9e5f
Mozammil <mozammiltarique007@gmail.com>
2025-05-03 19:31:03 +0530

    add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import logo from "/logo-icon.svg";
+
+function Navbar() {
+  return (
+    <nav className="navbar side-padding">
+      <div className="nav-content">
+        <div className="logo-and-title">
+          <img src={logo} className="logo" />
+          <p className="title">EduSphere</p>
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>Program</li>
+          <li>About Us</li>
+          <li>Campus</li>
+          <li>Testimonials</li>
+          <button className="btn">Contact Us</button>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
