11ec72990afce099d9c263ccc669acce12e6bbbe
Mozammil <mozammiltarique007@gmail.com>
2025-05-05 02:25:45 +0530

    Add Title Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Programs from "./components/Programs";
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Title subtitle="OUR PROGRAM" title="What We Offer" />
       <Programs />
     </>
   );
---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+function Title({subtitle, title}) {
+  return (
+    <div className="title center">
+      <p className="title-p">{subtitle}</p>
+      <h2 className="title-h">{title}</h2>
+    </div>
+  );
+}
+
+export default Title;
