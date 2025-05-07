86e16ff43523a4032c5c875ef3b1e454294038e7
Mozammil <mozammiltarique007@gmail.com>
2025-05-08 01:34:17 +0530

    Installed React Hot Toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-  import Navbar from "./components/Navbar";
-  import Hero from "./components/Hero";
-  import Programs from "./components/Programs";
-  import Title from "./components/Title";
-  import About from "./components/About";
-  import Campus from "./components/Campus";
-  import Testimonials from "./components/Testimonials";
-  import Contact from "./components/Contact";
-  // import Testimonials_Test from "./components/Testimonials-test";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Programs from "./components/Programs";
+import Title from "./components/Title";
+import About from "./components/About";
+import Campus from "./components/Campus";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
 
-  function App() {
-    return (
-      <>
-        <Navbar />
-        <Hero />
-        <Title subtitle="OUR PROGRAM" title="What We Offer" />
-        <Programs />
-        <About />
-        <Title subtitle="GALLERY" title="Campus Photos" />
-        <Campus />
-        <Title subtitle="TESTIMONIALS" title="What Student Says" />
-        <Testimonials />
-        <Title subtitle="CONTACT US" title="Get In Touch" />
-        <Contact />
-        {/* <Testimonials_Test /> */}
-      </>
-    );
-  }
+// import Testimonials_Test from "./components/Testimonials-test";
 
-  export default App;
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Title subtitle="OUR PROGRAM" title="What We Offer" />
+      <Programs />
+      <About />
+      <Title subtitle="GALLERY" title="Campus Photos" />
+      <Campus />
+      <Title subtitle="TESTIMONIALS" title="What Student Says" />
+      <Testimonials />
+      <Title subtitle="CONTACT US" title="Get In Touch" />
+      <Contact />
+      <Toaster />
+      {/* <Testimonials_Test /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,75 @@
+import message_icon from "/msg-icon.png";
+import mail_icon from "/mail-icon.png";
+import phone_icon from "/phone-icon.png";
+import location_icon from "/location-icon.png";
+import whiteArrowIcon from "/white-arrow.png";
+
+function Contact() {
+  return (
+    <div className="contact center">
+      <div className="contact-col">
+        <h3 className="contact-col-h3 icon-text">
+          Send us a message
+          <img src={message_icon} className="w-10" />
+        </h3>
+        <p className="contact-para  mb-5">
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <p className="contact-para icon-text">
+          <img src={mail_icon} className="icon" /> contact@edusphere.com
+        </p>
+        <p className="contact-para icon-text">
+          <img src={phone_icon} className="icon" /> 1800-465-429
+        </p>
+        <p className="contact-para icon-text">
+          <img src={location_icon} className="icon" /> 12B Palm Avenue, Kolkata
+          <br /> Kol-700091, India
+        </p>
+      </div>
+      <div className="contact-col">
+        <form className="form" onSubmit={onSubmit}>
+          <label className="label">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            className="input-area"
+            required
+          />
+          <label className="label">Phone Number</label>
+          <input
+            type="tel"
+            name="number"
+            placeholder="Enter your contact number"
+            className="input-area"
+            required
+          />
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            className="input-area"
+            required
+          />
+          <label className="label">Message</label>
+          <textarea
+            name="message"
+            cols="30"
+            rows="6"
+            placeholder="Enter your message"
+            className="input-area"
+          />
+          <button className="btn dark py-4 px-7 text-lg">
+            Submit Now <img src={whiteArrowIcon} className="arrow-icon" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
