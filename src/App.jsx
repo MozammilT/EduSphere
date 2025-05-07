16b2e701d0eef9d67f71b128c40349ee1d9b30be
Mozammil <mozammiltarique007@gmail.com>
2025-05-07 23:58:45 +0530

    Add Contact Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Programs from "./components/Programs";
-import Title from "./components/Title";
-import About from "./components/About";
-import Campus from "./components/Campus";
-import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
-// import Testimonials_Test from "./components/Testimonials-test";
+  import Navbar from "./components/Navbar";
+  import Hero from "./components/Hero";
+  import Programs from "./components/Programs";
+  import Title from "./components/Title";
+  import About from "./components/About";
+  import Campus from "./components/Campus";
+  import Testimonials from "./components/Testimonials";
+  import Contact from "./components/Contact";
+  // import Testimonials_Test from "./components/Testimonials-test";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Title subtitle="OUR PROGRAM" title="What We Offer" />
-      <Programs />
-      <About />
-      <Title subtitle="GALLERY" title="Campus Photos" />
-      <Campus />
-      <Title subtitle="TESTIMONIALS" title="What Student Says" />
-      <Testimonials />
-      <Title subtitle="CONTACT US" title="Get In Touch" />
-      <Contact />
-      {/* <Testimonials_Test /> */}
-    </>
-  );
-}
+  function App() {
+    return (
+      <>
+        <Navbar />
+        <Hero />
+        <Title subtitle="OUR PROGRAM" title="What We Offer" />
+        <Programs />
+        <About />
+        <Title subtitle="GALLERY" title="Campus Photos" />
+        <Campus />
+        <Title subtitle="TESTIMONIALS" title="What Student Says" />
+        <Testimonials />
+        <Title subtitle="CONTACT US" title="Get In Touch" />
+        <Contact />
+        {/* <Testimonials_Test /> */}
+      </>
+    );
+  }
 
-export default App;
+  export default App;
